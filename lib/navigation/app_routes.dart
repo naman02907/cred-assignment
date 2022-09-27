acb01e333a4af0004d83d1924e93cc7f84848f87
Naman <naman.sinsinwar@bitclass.live>
2022-09-28 00:10:29 +0530

    removed unnecessary navigation

--- lib/navigation/app_routes.dart
+++
@@ -1,7 +0,0 @@
-class AppRoutes {
-  static const String homePage = "/homePage";
-  static const String bottomSheet1 = "bottom_sheet_1";
-  static const String bottomSheet2 = "bottom_sheet_2";
-  static const String bottomSheet3 = "bottom_sheet_3";
-  static const String bottomSheet4 = "bottom_sheet_4";
-}
